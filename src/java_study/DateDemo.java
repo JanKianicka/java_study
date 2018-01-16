dee6b403afa86472cd1d4544190bb0be5e3cbdfe
janki <janki@nod293.ad.mstep>
2018-01-16 14:28:05 +0100

    Store Date variable test

---
+++ src/java_study/DateDemo.java
@@ -0,0 +1,16 @@
+package java_study;
+
+import java.util.Date;
+
+public class DateDemo {
+
+   public static void main(String args[]) {
+      // Instantiate a Date object
+	  // gets current date
+      Date date = new Date();
+
+      // display time and date using toString()
+      System.out.println(date.toString());
+   }
+}
+
