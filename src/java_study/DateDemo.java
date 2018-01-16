3b0f20d423ff6be29b5872c53db892e177364106
janki <janki@nod293.ad.mstep>
2018-01-16 14:51:08 +0100

    Study of Date class and useful examples.

--- src/java_study/DateDemo.java
+++ src/java_study/DateDemo.java
@@ -1,5 +1,6 @@
 package java_study;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateDemo {
@@ -7,10 +8,25 @@ public class DateDemo {
    public static void main(String args[]) {
       // Instantiate a Date object
 	  // gets current date
-      Date date = new Date();
-
+	  Date date = new Date();
+      
       // display time and date using toString()
       System.out.println(date.toString());
+      
+      // Formater
+      SimpleDateFormat ft = 
+    	      new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+      
+      System.out.println("Current Date: " + ft.format(date));
+      
+      // Epoch time handling
+      long currentEpoch = date.getTime()/1000; // divided by 1000 to get seconds from mseconds
+      System.out.println("Current date in Epoch: " + currentEpoch);
+      
+      // In linux you can get these times using date command
+      // date +%s     to get current epoch
+      // date -d @1516109832      to get date from epoch in seconds
+      
    }
 }
 
