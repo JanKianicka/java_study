c3b0f6565d75d5fd0b56d7bdb365eb5af86c7e0b
Jan Kianicka <jan.kianicka@microstep-mis.com>
2018-03-14 10:36:32 +0100

    Testing of regexp with different datetime patterns.

--- src/java_study/RegexMatches.java
+++ src/java_study/RegexMatches.java
@@ -37,20 +37,28 @@ public class RegexMatches {
    
    public static void testMetarTAFStrings() {
 	   String metarIn = "METAR LZTT 081400Z 25012KT 9999 FEW010 OVC046 03/M01 Q1006 NOSIG=";
+	   // String metarIn = "MET REPORT LZIB 141130Z AUTO WIND RWY 31 TDZ 290/8KT VRB BTN 260/ AND 320/ RWY 22 END 300/10KT VRB BTN 270/ AND 340/ VIS 10KM CLD NSC T23 DP12 QNH 1017HPA TREND NOSIG ";
 	   String TAF_In = "SASQ LZTT 081400";
+	   String Inserter_sample = "AWSSTAT,018,2018-03-11 00:02:26\\n018,2018-03-11 00:02:26,startTime=2018-03-11 00:02:23,connectTime=2018-03-11 00:02:23,l\r\n" + 
+	   		"astReplyTime=2018-03-11 00:02:26,endTime=2018-03-11 00:02:26,tries=0,cq=9.999993,statusCode=OK,messages=Downloaded 1 new@histOrNew 'mlg' records.,err=null,connectDeta\r\n" + 
+	   		"il=null";
 	   
-	   String dayPattern = "(0?[1-9]|[12][0-9]|3[01])";
-	   String minuteSecondPattern = "(0?[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|6[0])";
+	   String dayPattern = "(0[1-9]|[12][0-9]|3[01])";
+	   String hourPattern = "(0[0-9]|1[0-9]|2[0-9])";
+	   String minuteSecondPattern = "(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|6[0])";
+	   String inserterPattern = "AWSSTAT";
 	   
-	   String metarPattern = String.format(" %s%s%sZ ", dayPattern, minuteSecondPattern, minuteSecondPattern);
-	   String tafPattern = String.format(" %s%s%s", dayPattern, minuteSecondPattern, minuteSecondPattern);
+	   String metarPattern = String.format(" %s%s%s", dayPattern, hourPattern, minuteSecondPattern);
+	   String tafPattern = String.format(" %s%s%s", dayPattern, hourPattern, minuteSecondPattern);
 	   
 	   System.out.println("Matching pattern for metar's DDhhmmZ:" + metarPattern);
 	   Pattern rMetar = Pattern.compile(metarPattern);
 	   Pattern rTaf = Pattern.compile(tafPattern);
+	   Pattern rInserterPattern = Pattern.compile(inserterPattern);
 	   
 	   Matcher mMetar = rMetar.matcher(metarIn);
 	   Matcher mTaf = rTaf.matcher(TAF_In);
+	   Matcher mInserterPattern = rInserterPattern.matcher(Inserter_sample);
 	   
 	   // we replace with simple date formated datetime
 	   Date now = new Date();
@@ -58,11 +66,20 @@ public class RegexMatches {
 	    	      new SimpleDateFormat ("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 	   
 	   
-	   String mRet = mMetar.replaceFirst(String.format(" %s ",ft.format(now)));
+	   String mRet = mMetar.replaceFirst(String.format(" %s",ft.format(now)));
 	   String tRet = mTaf.replaceFirst(String.format(" %s", ft.format(now)));
 	   
 	   System.out.println(mRet);
 	   System.out.println(tRet);
+	   
+	   SimpleDateFormat ft2 = 
+	    	      new SimpleDateFormat ("ddHHmm");
+	   
+	   mRet = mMetar.replaceFirst(String.format(" %s ",ft2.format(now)));
+	   System.out.println(mRet);
+	   
+	   System.out.println("Replaced: " + mInserterPattern.replaceFirst("REPLACED"));
+	   
 	   
    }
    
@@ -71,9 +88,9 @@ public class RegexMatches {
 	   //System.out.print(readcastXMLstr.substring(0, 1000));
 	   
 	   String yearPattern = "((19|20)\\d\\d)";
-	   String monthPattern = "(0?[1-9]|1[012])";
-	   String dayPattern = "(0?[1-9]|[12][0-9]|3[01])";
-	   String minuteSecondPattern = "(0?[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|6[0])";
+	   String monthPattern = "(0[1-9]|1[012])";
+	   String dayPattern = "(0[1-9]|[12][0-9]|3[01])";
+	   String minuteSecondPattern = "(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|6[0])";
 	   
 	   // ISO_INSTANT: 2014-11-01T11:43:48.870Z regular expression to match the date
 	   String smallStringToTestPattern = "<first-roadcast>2018-03-08T13:15Z</first-roadcast>";
