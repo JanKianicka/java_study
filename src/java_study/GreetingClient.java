b09fe28c8180c236f812543b69e1cd125cc00015
Jan Kianicka <jan.kianicka@microstep-mis.com>
2018-06-28 09:44:28 +0200

    Progress on learning server/client sockets, working example.

--- src/java_study/GreetingClient.java
+++ src/java_study/GreetingClient.java
@@ -31,3 +31,37 @@ public class GreetingClient {
 	}
 
 }
+
+// Hurah, it has worked with local host
+// C:\repo\java_study\src>java java_study.GreetingServer 6066
+// and connecting client to 192.168.160.172 6066
+// This was the output:
+
+//Waiting for client on port 6066...
+//Just connected to /192.168.160.172:60784
+//Hello from /192.168.160.172:60784
+//Waiting for client on port 6066...
+//Just connected to /192.168.160.172:60785
+//Hello from /192.168.160.172:60785
+//Waiting for client on port 6066...
+//Just connected to /192.168.160.172:60787
+//Hello from /192.168.160.172:60787
+//Waiting for client on port 6066...
+//Just connected to /192.168.160.172:60788
+//Hello from /192.168.160.172:60788
+//Waiting for client on port 6066...
+//Just connected to /192.168.160.172:60789
+//Hello from /192.168.160.172:60789
+//Waiting for client on port 6066...
+//Just connected to /192.168.160.172:60790
+//Hello from /192.168.160.172:60790
+//Waiting for client on port 6066...
+//Just connected to /192.168.160.172:60791
+//Hello from /192.168.160.172:60791
+//Waiting for client on port 6066...
+//Just connected to /192.168.160.172:60792
+//Hello from /192.168.160.172:60792
+//Waiting for client on port 6066...
+//Socket timed out!
+//
+//C:\repo\java_study\src>
